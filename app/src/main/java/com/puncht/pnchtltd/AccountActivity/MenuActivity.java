3fade307fec451b0f63b2f7b37979e41ec1498e8
Kilo$$ <x16321103@student.ncirl.ie>
2018-04-01 20:43:26 +0100

    Initial commit

---
+++ app/src/main/java/com/puncht/pnchtltd/AccountActivity/MenuActivity.java
@@ -0,0 +1,15 @@
+package com.puncht.pnchtltd.AccountActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.puncht.pnchtltd.R;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
